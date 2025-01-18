a18eb618b06a1bb1fad6391993a5569f496eafc1
LuigiDotSRC <luigi.terashima@gmail.com>
2025-01-18 14:33:27 -0500

    implement decryption, result: `HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA`

--- python/solution.py
+++ python/solution.py
@@ -1,4 +1,6 @@
-def create_square(key: str):
+from typing import List
+
+def create_square(key: str) -> List[List[str]]:
     square = [['']*5 for i in range(5)]
 
     i, j = 0, 0
@@ -16,9 +18,38 @@ def create_square(key: str):
 
     return square
 
-if __name__ == "__main__":
+def find_char(char: str, square: List[List[str]]) -> tuple[int, int]:
+    for i in range(5):
+        for j in range(5):
+            if square[i][j] == char:
+                return (i,j)
+            
+def decrypt(ciphertext: str, square: List[List[str]]) -> str:
+    bigraphs = [ciphertext[i:i+2] for i in range(0, len(ciphertext), 2)]
+    result = []
+    
+    for b in bigraphs:
+        r1, c1 = find_char(b[0], square)
+        r2, c2 = find_char(b[1], square)
 
+        if r1 == r2:
+            result.append(square[r1][(c1 - 1) % 5])
+            result.append(square[r2][(c2 - 1) % 5])
+
+        elif c1 == c2:
+            result.append(square[(r1 - 1) % 5][c1])
+            result.append(square[(r2 - 1) % 5][c2])
+
+        else:
+            result.append(square[r1][c2])
+            result.append(square[r2][c1])
+
+    result = [c for c in result if c != 'X']
+    return "".join(result)
+
+if __name__ == "__main__":
     key = "SUPERSPY"
     ciphertext = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
 
-    print(create_square(key))
+    square = create_square(key)
+    print(decrypt(ciphertext, square))
